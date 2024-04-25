525f4aaa962780063338f4ed817bb7bcb6f9546f
Tavera15 <joaquinramirez97@yahoo.com>
2024-04-25 01:54:04 -0500

    added asterik to origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(App);
 
 const io = new Server(server, {
     cors: {
-      origins: 'https://effortless-starship-38a8e8.netlify.app/',
+      origins: ':*:',
       methods: ['GET', 'POST'],
       credentials: true,
       transports: ['websocket']
